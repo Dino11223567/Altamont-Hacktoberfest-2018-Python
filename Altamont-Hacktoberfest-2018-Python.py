143e98cd7ac46df1f3bc8b4d744c95cd94ab5880
Henry the Dinosaur <henry11223567@gmail.com>
2018-10-10 09:42:23 -0500

    Update Altamont-Hacktoberfest-2018-Python.py

--- Altamont-Hacktoberfest-2018-Python.py
+++ Altamont-Hacktoberfest-2018-Python.py
@@ -8,7 +8,7 @@ print("Hello, my name is Mary Allen.")
 teacher = "Mr. James"
 print("Hey there from {}".format(teacher))
 
-
+print("I am Henry. I want to sleep")
 print("Hello, my name is Rob Dominguez.")
 
 
